32838a5c5cd417d17944b0d6b2215a19728ecb24
Hariram <hariram6674@gmail.com>
2023-10-09 11:00:21 +0400

    another one

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const bool = false;
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    let d = new Date();
+    let today = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear(); 
+    res.render("index.ejs",{today:today});
+});
+
+app.get("/work",(req,res)=>{
+    res.render("index1.ejs");
+});
+
+
+
+app.listen(port,(req,res)=>{
+    console.log(`Server started in ${port}`);
+});
