e53acf2a58e2e3fcdbd6e30e03087fd252b5623b
Hariram-6674 <135815051+Hariram-6674@users.noreply.github.com>
2023-10-18 19:48:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import path from 'path';
 const app = express();
-app.set('views', './views');
+
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 const port = 3000;
 
