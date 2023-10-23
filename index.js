437b9653ae934af22f5bca1285a300bcf8a17c32
Hariram-6674 <135815051+Hariram-6674@users.noreply.github.com>
2023-10-23 13:14:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set("views", path.join(__dirname, "views"));
+
 app.set('view engine', 'ejs');
 const port = 3000;
 
